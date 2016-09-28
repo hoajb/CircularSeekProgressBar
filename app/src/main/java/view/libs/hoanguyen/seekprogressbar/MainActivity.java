7a9e6d53f7e680c62823de4102e3f27c640c0604
Hoa Nguyen <hoa.nguyenminh.it@gmail.com>
2016-09-28 11:52:23 +0700

    V2 summit

--- app/src/main/java/view/libs/hoanguyen/seekprogressbar/MainActivity.java
+++ app/src/main/java/view/libs/hoanguyen/seekprogressbar/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Comment test for new linux android studio
+        //V2
     }
 }
